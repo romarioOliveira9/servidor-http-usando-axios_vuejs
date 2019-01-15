5db7ce6a54468d54a280925661489b7e5eb84034
Plinio Naves <pliniopjn@hotmail.com>
2019-01-15 18:50:46 -0200

    Requisição GET para buscar lista de Tarefas

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -19,6 +19,9 @@
 
 <script>
 
+import axios from 'axios'
+
+import config from './../config/config'
 import TarefaSalvar from './TarefaSalvar.vue'
 import TarefasListaIten from './TarefasListaIten.vue'
 
@@ -29,12 +32,15 @@ export default {
     },
     data() {
         return {
-            tarefas: [
-                { id: 1, titulo: 'Aprender JavaScript', concluido: true },        
-                { id: 2, titulo: 'Aprender Vue', concluido: true },
-                { id: 3, titulo: 'Aprender Axios', concluido: false }
-            ]
+            tarefas: []
         }
+    },
+    created() {
+        axios.get(`${config.apiURL}/tarefas`)
+            .then((response) => {
+                console.log(response)
+                this.tarefas = response.data
+            })
     }
 }
 </script>
