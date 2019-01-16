8ce3615d9576924f1ee9980f510d7b87c425e48c
Plinio Naves <pliniopjn@hotmail.com>
2019-01-16 11:19:32 -0200

    Requisição POST para cadastrar nova Tarefa

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
 
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de Tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button 
+                    class="btn btn-primary float-right"
+                    @click="exibirFormulario = !exibirFormulario">
+                        <i class="fa fa-plus mr-2"></i>
+                        <span>Criar</span>
+                </button>
+            </div>
+        </div>
 
         <ul class="list-group" v-if="tarefas.length > 0">
             <TarefasListaIten
@@ -12,7 +24,9 @@
 
         <p v-else>Nenhuma tarefa criada.</p>
 
-        <TarefaSalvar />
+        <TarefaSalvar
+            v-if="exibirFormulario"
+            @criar="criarTarefa" />
 
     </div>
 </template>
@@ -32,15 +46,26 @@ export default {
     },
     data() {
         return {
-            tarefas: []
+            tarefas: [],
+            exibirFormulario: false
         }
     },
     created() {
         axios.get(`${config.apiURL}/tarefas`)
             .then((response) => {
-                console.log(response)
+                console.log('GET /tarefas', response)
                 this.tarefas = response.data
             })
+    },
+    methods: {
+        criarTarefa(tarefa) {
+            axios.post(`${config.apiURL}/tarefas`, tarefa)
+                .then((response) => {
+                    console.log('POST /tarefas', response)
+                    this.tarefas.push(response.data)
+                    this.exibirFormulario = false
+                })
+        }
     }
 }
 </script>
