c82920f9e190c01731ae732609ad9630de7a3562
Plinio Naves <pliniopjn@hotmail.com>
2019-01-17 14:44:50 -0200

    Enviando cabeçalhos na requisição

--- src/axios.js
+++ src/axios.js
@@ -6,19 +6,26 @@ const instance = axios.create({})
 instance.defaults.baseURL = config.apiURL
 
 instance.interceptors.request.use(config => {
+
     console.log('Interceptando requisição: ', config)
+
     config.data = {
         ...config.data,
         curso: 'Vue JS'
     }
-    // return config
-    return new Promise((resolve, reject) => {
+
+    config.headers.common['Authorization'] = `Bearer token_jwt`
+    config.headers.put['Meu-Cabecalho'] = 'Curso VueJS'
+
+    return config
+
+    /*return new Promise((resolve, reject) => {
         console.log('Fazendo requisição aguardar...')
         setTimeout(() => {
             console.log('Enviando requisição...')
             resolve(config)
         }, 2000)
-    })
+    })*/
 }, error => {
     console.log('Erro ao fazer requisição: ', error)
     return Promise.reject(error)
