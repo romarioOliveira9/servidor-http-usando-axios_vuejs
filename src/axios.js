79711b9bdb52055e2469ed02abf0437808e03ced
Plinio Naves <pliniopjn@hotmail.com>
2019-01-17 13:33:20 -0200

    Criando instância do axios com valores padrões

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import config from './config/config'
 
-axios.defaults.baseURL = config.apiURL
+const instance = axios.create({})
 
-export default axios
+instance.defaults.baseURL = config.apiURL
+
+export default instance
