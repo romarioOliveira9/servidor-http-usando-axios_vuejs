6fce35b8e8c7d9c28ff1582aad05405d5df183c6
Plinio Naves <pliniopjn@hotmail.com>
2019-01-17 17:14:06 -0200

    Bônus: acompanhando progresso de Download

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,28 @@
 
       <TarefasLista />
 
+      <button 
+        class="btn btn-primary mt-4 mb-2"
+        @click="download">
+          Baixar Imagem
+      </button>
+
+      <div class="progress">
+        <div 
+          class="progress-bar" 
+          role="progressbar" 
+          :style="{ width: progresso + '%' }" 
+          :aria-valuenow="progresso" 
+          aria-valuemin="0" 
+          aria-valuemax="100">
+            {{ progresso }} %
+        </div>
+      </div>
+
+      <div v-if="imagem">
+        <img :src="imagem" style="max-width: 100%">
+      </div>
+
     </div>
 
   </div>
@@ -27,6 +49,12 @@ import TarefasLista from './components/TarefasLista.vue'
 export default {
     components: {
         TarefasLista
+    },
+    data() {
+      return {
+        progresso: 0,
+        imagem: undefined
+      }
     },
     async created() {
       /*axios.all([
@@ -55,6 +83,30 @@ export default {
       console.log('Tarefa 1: ', tarefa1)
       console.log('Tarefa 3: ', tarefa3)
 
+    },
+    methods: {
+      download(event) {
+        axios.get(
+          'https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          {
+            responseType: 'blob',
+            onDownloadProgress: (progressEvent) => {
+              console.log('Fazendo download...', progressEvent)
+              this.progresso = (progressEvent.loaded / progressEvent.total * 100).toFixed(0)
+            }
+          }
+        ).then(response => {
+
+          console.log('Download concluído!', response)
+
+          const reader = new window.FileReader()
+          reader.readAsDataURL(response.data)
+          reader.onload = () => {
+            this.imagem = reader.result
+          }
+
+        })
+      }
     }
 }
 </script>
