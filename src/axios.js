18f622dfc56d40b88814c6806dcbd1669b9a9d5f
Plinio Naves <pliniopjn@hotmail.com>
2019-01-17 14:10:30 -0200

    Interceptando requisições

--- src/axios.js
+++ src/axios.js
@@ -5,4 +5,23 @@ const instance = axios.create({})
 
 instance.defaults.baseURL = config.apiURL
 
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisição: ', config)
+    config.data = {
+        ...config.data,
+        curso: 'Vue JS'
+    }
+    // return config
+    return new Promise((resolve, reject) => {
+        console.log('Fazendo requisição aguardar...')
+        setTimeout(() => {
+            console.log('Enviando requisição...')
+            resolve(config)
+        }, 2000)
+    })
+}, error => {
+    console.log('Erro ao fazer requisição: ', error)
+    return Promise.reject(error)
+})
+
 export default instance
